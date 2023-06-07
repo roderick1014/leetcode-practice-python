b7b919f45d9e8a5bd73315269cacf0e741580070
Roderick <73574008+roderick1014@users.noreply.github.com>
2023-06-07 18:56:00 +0800

    Add files via upload

---
+++ 0012_integer2Roman.py
@@ -0,0 +1,56 @@
+# 0012 - Integer to Roman
+
+'''
+    Question:
+        Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+        Symbol       Value
+        I             1
+        V             5
+        X             10
+        L             50
+        C             100
+        D             500
+        M             1000
+        For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+        Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+        I can be placed before V (5) and X (10) to make 4 and 9.
+        X can be placed before L (50) and C (100) to make 40 and 90.
+        C can be placed before D (500) and M (1000) to make 400 and 900.
+        Given an integer, convert it to a roman numeral.
+'''
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+
+        num_map = {
+            1: "I",
+            5: "V",    4: "IV",
+            10: "X",   9: "IX",
+            50: "L",   40: "XL",
+            100: "C",  90: "XC",
+            500: "D",  400: "CD",
+            1000: "M", 900: "CM",
+        }
+
+        # Result Variable
+        result = ''
+
+        for val in [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]:
+            # If n in list then add the roman value to result variable
+            while num >= val:
+                result += num_map[val]
+                num -= val
+        return result
+
+if __name__ == '__main__':
+    sol = Solution()
+    num = 58
+    num = 1994
+    print(sol.intToRoman(num))
